3b9403c4224749c8f53675edda91a3798a3a3679
Vincent Saelzler <vincent.saelzler@gmail.com>
2018-12-06 22:44:05 -0500

    copy updates, footer, license

--- Pages/About.cshtml.cs
+++ Pages/About.cshtml.cs
@@ -8,11 +8,5 @@ namespace CollatzCoreRazorPage.Pages
 {
     public class AboutModel : PageModel
     {
-        public string Message { get; set; }
-
-        public void OnGet()
-        {
-            Message = "Your application description page.";
-        }
     }
 }
--- Pages/Contact.cshtml.cs
+++ Pages/Contact.cshtml.cs
@@ -8,11 +8,5 @@ namespace CollatzCoreRazorPage.Pages
 {
     public class ContactModel : PageModel
     {
-        public string Message { get; set; }
-
-        public void OnGet()
-        {
-            Message = "Your contact page.";
-        }
     }
 }
